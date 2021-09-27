505594becb37f54944f8db6e0445603b4b6d2980
justinqualley <54243022+justinqualley@users.noreply.github.com>
2021-09-27 05:33:46 -0400

    added flags for array

--- project/pa1.c
+++ project/pa1.c
@@ -2,20 +2,44 @@
 #include <stdbool.h>
 #include <stdlib.h> 
 #include <string.h>
+#include <unistd.h>
 #include "list_of_list.h"
 #include "shell_array.h"
 #include "shell_list.h"
 int main(int argc, char * * argv){
-    if(argc != 3){
+    if(argc != 4){
         return EXIT_FAILURE;
+    }
+    int c;
+    int aflag = 0;
+    int lflag = 0;
+    while((c = getopt(argc, argv, "al:")) != -1){
+        switch (c){
+            case 'a':
+                aflag = 1;
+                break;
+            case 'l':
+                lflag = 1;
+                break;
+      default:
+        return EXIT_FAILURE;
+      }
     }
     int size;
     long n_comp = 0;
     long *arr;
     int written;
-    arr = Array_Load_From_File(argv[1], &size);
-    Array_Shellsort(arr, size, &n_comp);
-    written = Array_Save_To_File(argv[2], arr, size);
+    if(aflag == 1){
+        arr = Array_Load_From_File(argv[2], &size);
+        Array_Shellsort(arr, size, &n_comp);
+        written = Array_Save_To_File(argv[3], arr, size);
+        size = written;
+    }else if(lflag == 1){
+        //arr = List_Load_From_File(argv[2]);
+        //List_Shellsort(arr, &n_comp);
+        //written = List_Save_To_File(argv[3], arr);
+    }
+    
     return EXIT_SUCCESS;
 }
 
